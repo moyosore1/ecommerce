6ea3e7cd6965a89a9c8c45e1f40b8a806c338a76
Moyosore1 <moyosoreolumideobi@gmail.com>
2020-09-18 13:59:21 +0100

    Third commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn msecommerce-wsgi --log-file -
--- msecommerce/settings.py
+++ msecommerce/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')h9htteej*!3kp^a8)!+p8-uq=(081n+2+%mroxv#oi5cfoy*)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sherrifmoyo.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

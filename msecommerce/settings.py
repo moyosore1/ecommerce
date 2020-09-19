39f30186271c7789e07ae0c4fa000b4edf264de1
Moyosore1 <moyosoreolumideobi@gmail.com>
2020-09-19 14:57:17 +0100

    Finale Commit

--- msecommerce/settings.py
+++ msecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')h9htteej*!3kp^a8)!+p8-uq=(081n+2+%mroxv#oi5cfoy*)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['sherrifmoyo.herokuapp.com', '127.0.0.1']
 
